7fd59550ea7ff1f83dffbd2236f41604c9d2340f
USER <user@AL01511954.local>
2022-04-15 01:07:38 +0900

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to the about page</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    let count = 0;
+
+    function incrementBtn() {
+        count += 1;
+    }
+</script>
+
+<h1>Number: {count}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/about">hihi</a>
+<button on:click={incrementBtn}>Increment</button>
+<style>
+    p {
+        font-size: 2rem;
+    }
+</style>
